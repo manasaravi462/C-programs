43445820fe3128370b061fda99146e95e4f60092
manasaravi462 <97960729+manasaravi462@users.noreply.github.com>
2023-08-19 12:30:41 +0530

    Add files via upload

---
+++ p11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=12;
+    float b=1.78;
+    char str='m';
+    long int c=99999999;
+    unsigned long int d=9839839383938;
+    printf("Integer:%d\nfloat:%f\ncharacter:%c\nlong int:%ld\nunsigned:%lu\n",a,b,str,c,d);
+    return 0;
+}
