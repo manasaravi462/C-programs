4a25f0687f6b9034a5d6aae70959e5657111ebb6
manasaravi462 <97960729+manasaravi462@users.noreply.github.com>
2023-08-20 10:07:52 +0530

    Add files via upload

---
+++ p17.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=98;
+    float b=3.765;
+    printf("length is %d \n",a);
+    printf("width is %4.2f \n",b);
+    printf("precision to .6 is %.6f \n",b);
+    return 0;
+}
