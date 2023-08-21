1853d24ad7b56bf6ddb00a34ec027e6ab6c1cfab
manasaravi462 <97960729+manasaravi462@users.noreply.github.com>
2023-08-21 12:17:17 +0530

    Add files via upload

---
+++ p25.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    const int var=7654;
+    var=908;
+    printf("%d",var);
+    return 0;
+}
---
+++ p26.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    const var=7654;
+    printf("%d",var);
+    return 0;
+}
